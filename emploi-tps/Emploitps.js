e8f8a623f343126987638ea05fb0112bedefa298
Mael <legrand.mael2002@gmail.com>
2020-11-17 14:44:56 +0100

    Add pages

---
+++ emploi-tps/Emploitps.js
@@ -0,0 +1,108 @@
+let date = new Date();
+let nbday = date.getDay();
+let day = returnDay(nbday);
+let mois = date.getMonth();
+let month = returnMonth(mois);
+let year = date.getFullYear();
+let jourDuMois = date.getDate();
+console.log(month);
+
+
+let board = []
+
+
+
+
+
+document.getElementById("date").innerHTML = day+" "+jourDuMois+" "+month+" "+year;
+
+
+
+function neweventDisplay(){
+	document.getElementById("divNewEvent").style.display = "block";
+}
+
+
+function newevent(){
+	let eventDate;	//normalement type date
+	let eventTime;
+	let eventText;
+	eventDate = document.getElementById("eventDate").value;
+	eventTime = document.getElementById("eventTime").value;
+	eventText = document.getElementById("eventText").value;
+	
+	if (eventDate === ""){
+		document.getElementById("erreur").innerHTML = "Veuillez entrer une date!";
+		
+	}else if (eventTime === ""){
+		document.getElementById("erreur").innerHTML = "Veuillez entrer une heure!";
+	}else if (eventText == "") {
+		document.getElementById("erreur").innerHTML = "Veuillez entrer un evenement";
+	}else{
+		document.getElementById("erreur").innerHTML = "";
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+function returnDay(nb){
+	let jour;
+	if(nb == 1)
+	{
+		jour = "lundi";
+	}else if(nb == 2){
+		jour = "mardi";
+	}else if(nb == 3){
+		jour = "mercredi";
+	}else if(nb == 4){
+		jour = "jeudi";
+	}else if(nb == 5){
+		jour = "vendredi";
+	}else if(nb == 6){
+		jour = "samedi";
+	}else if(nb == 7){
+		jour = "dimanche";
+	}
+	return jour;
+}
+
+
+
+function returnMonth(nb){
+	let jour;
+	if(nb == 0)
+	{
+		jour = "janvier";
+	}else if(nb == 1){
+		jour = "fevrier";
+	}else if(nb == 2){
+		jour = "mars";
+	}else if(nb == 3){
+		jour = "avril";
+	}else if(nb == 4){
+		jour = "mai";
+	}else if(nb == 5){
+		jour = "juin";
+	}else if(nb == 6){
+		jour = "juillet";
+	}else if(nb == 7){
+		jour = "aout";
+	}else if(nb == 8){
+		jour = "septembre";
+	}else if(nb == 9){
+		jour = "octobre";
+	}else if(nb == 10){
+		jour = "novembre";
+	}else if(nb == 11){
+		jour = "decembre";
+	}
+	return jour;
+}
