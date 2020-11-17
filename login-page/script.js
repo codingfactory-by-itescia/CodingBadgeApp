d2f8e0fe6705e8340cee40beb85282bae3d98459
Mael <legrand.mael2002@gmail.com>
2020-11-17 09:57:31 +0100

    Ajout login-Page

---
+++ login-page/script.js
@@ -0,0 +1,23 @@
+let actualPage = "home";
+
+function displayPage(page){
+    document.getElementById(actualPage).style.display = "none";
+    let pageToDisplay = document.getElementById(page);
+    pageToDisplay.style.display = "flex";
+    actualPage = page;
+}
+
+function validateLogin(){
+    let identifiant = $("#unmame").val();
+    let password = $("#password").val();
+    if(identifiant == 'root' || password == 'root'){
+        window.location = "../index.html";
+    } else{
+        $('#unmame').css("border-bottom","2px solid #ff0000");
+        $('#password').css("border-bottom","2px solid #ff0000");
+        setTimeout(function(){
+            $('#unmame').css("border-bottom","2px solid #404040");
+            $('#password').css("border-bottom","2px solid #404040");
+        },500);
+    }
+}
